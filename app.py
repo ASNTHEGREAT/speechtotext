67869697b83f83a3af3021513f3e81281db51773
ASNTHEGREAT <aditya.nittala5@gmail.com>
2022-10-18 19:23:22 +0530

    many bugs cleared (expect post method, it still works)

--- app.py
+++ app.py
@@ -4,18 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 # create an instance of flask
 app = Flask(__name__)
-# create an instance of API
+# creating an API object
 api = Api(app)
 # create database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stt.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///speechtotext.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # sqlalchemy mapper
 db = SQLAlchemy(app)
 
 
-
 # add a class
-class speechtotext(db.Model):
+class SpeechToText(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fooditemname = db.Column(db.String(80), nullable=False)
     fooditemprice = db.Column(db.Float, nullable=False)
@@ -23,35 +22,37 @@ class speechtotext(db.Model):
     def __repr__(self):
         return f"{self.fooditemname} - {self.fooditemprice}"
 
-#For GET request to http://localhost:5000/
 
+# For GET request to http://localhost:5000/
 class GetItem(Resource):
     def get(self):
-        items = speechtotext.query.all()
+        items = SpeechToText.query.all()
         item_list = []
-        for item in speechtotext:
-            item_data = {'Id': item.id, 'FoodItemName': item.fooditemname, 'FoodItemPrice': item.fooditemprice}
+        for i in items:
+            item_data = {'Id': i.id, 'FoodItemName': i.fooditemname, 'FoodItemPrice': i.fooditemprice}
             item_list.append(item_data)
         return {"Items": item_list}, 200
 
-# For POST request to http://localhost:5000/item
+
+# For Post request to http://localhost:5000/item
 class AddItem(Resource):
     def post(self):
         if request.is_json:
-            item = speechtotext(fooditemname=request.json["FoodItemName"], fooditemprice=request.json["FoodItemPrice"])
+            item = SpeechToText(fooditemname=request.json['FoodItemName'], fooditemprice=request.json['FoodItemPrice'])
             db.session.add(item)
             db.session.commit()
             # return a json response
-            return make_response(jsonify({'Id': item.id, 'FoodItemName': item.fooditemname,
-                                          'FoodItemPrice': item.fooditemprice}), 201)
+            return make_response(
+                jsonify({'Id': item.id, 'Food Item Name': item.fooditemname, 'Food Item Price': item.fooditemprice}), 201)
         else:
             return {'error': 'Request must be JSON'}, 400
 
-# For PUT request to http://localhost:5000/update/?
+
+# For put request to http://localhost:5000/update/?
 class UpdateItem(Resource):
     def put(self, id):
         if request.is_json:
-            item = speechtotext.query.get(id)
+            item = SpeechToText.query.get(id)
             if item is None:
                 return {'error': 'not found'}, 404
             else:
@@ -62,23 +63,23 @@ class UpdateItem(Resource):
         else:
             return {'error': 'Request must be JSON'}, 400
 
-# For DELETE request to http://localhost:5000/delete/?
+
+# For delete request to http://localhost:5000/delete/?
 class DeleteItem(Resource):
     def delete(self, id):
-        item = speechtotext.query.get(id)
+        item = SpeechToText.query.get(id)
         if item is None:
-            return {'error': 'Not found'}, 404
+            return {'error': 'not found'}, 404
         db.session.delete(item)
         db.session.commit()
         return f'{id} is deleted', 200
 
+
 api.add_resource(GetItem, '/')
 api.add_resource(AddItem, '/add')
-api.add_resource(UpdateItem, '/update/<int:int>')
-api.add_resource(DeleteItem, '/delete/<int:int>')
+api.add_resource(UpdateItem, '/update/<int:id>')
+api.add_resource(DeleteItem, '/delete/<int:id>')
 
+#
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
